ed531ac513b27693ce2ae45c40b9ea485d463ac7
uri-karpenkop <uri.karpenkop@gmail.com>
2021-03-30 18:10:19 +0300

    git ignore 2

---
+++ app/src/main/java/com/example/basketballtrain/AllDribellingWorkouts.java
@@ -0,0 +1,32 @@
+package com.example.basketballtrain;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class AllDribellingWorkouts extends AppCompatActivity {
+
+    Button btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.all_dribelling_workout_activity);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        btnBack = (Button) findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                if (v == btnBack) {
+                    finish();
+                }
+            }
+
+        });
+    }
+}
