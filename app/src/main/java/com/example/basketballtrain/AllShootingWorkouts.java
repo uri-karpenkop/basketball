7090db7fc8b910e6c2f625fd03aebbbc88ac60b5
uri-karpenkop <uri.karpenkop@gmail.com>
2021-03-30 18:08:04 +0300

    git ignore 2

--- app/src/main/java/com/example/basketballtrain/AllShootingWorkouts.java
+++ app/src/main/java/com/example/basketballtrain/AllShootingWorkouts.java
@@ -1,18 +1,22 @@
 package com.example.basketballtrain;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 public class AllShootingWorkouts extends AppCompatActivity {
+
     Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.all_shooting_workouts_activity);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         btnBack = (Button) findViewById(R.id.btnBack);
         btnBack.setOnClickListener(new View.OnClickListener() {
